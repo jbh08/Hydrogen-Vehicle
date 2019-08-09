b253e0f1ebe8f448e88f9a98875facd049fae19b
JH <wlgurdlcjswo@naver.com>
2019-08-09 12:10:13 +0900

    update layout

--- layout/app.py
+++ layout/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('analysis.html')
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
 
 if __name__ == '__main__':
     app.run()
